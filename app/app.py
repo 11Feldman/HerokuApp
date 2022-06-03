18147621f5f9313cef00f101c3e14b404daa78a4
11Feldman <44484762+11Feldman@users.noreply.github.com>
2022-06-03 00:31:34 -0300

    Primer commit

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask,jsonify,request
+from sklearn.datasets import load_iris
+import joblib
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Creando la primera pagina'
+
+@app.route('/predecir')
+def predecir():
+    # json=request.get_json()
+    # medidas=json['Medidas']
+    # clf = joblib.load('modelo_entrenado.pkl')
+    # prediccion = clf.predict(medidas)
+    # return 'Las medidas que diste corresponde a la clase {0}'.format(prediccion)
+    return 'Las medidas que diste corresponde a la clase'
 
 if __name__ == '__main__':
     app.run()
